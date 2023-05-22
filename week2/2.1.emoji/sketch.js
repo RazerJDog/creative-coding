3ec18ad00966f09a1ea3d7822d70a712206672e5
RazerJDog <JUSTIN.RAZE.50@GMAIL.COM>
2023-05-22 14:33:34 -0400

    Emoji Assignment completed

--- week2/2.1.emoji/sketch.js
+++ week2/2.1.emoji/sketch.js
@@ -4,4 +4,21 @@ function setup() {
 
 function draw() {
   background(220);
+  stroke("gold"); // set the stroke (line) color 
+  strokeWeight(6); // set the stroke thickness
+  fill("yellow"); // set the fill color
+
+  // draw an ellipse with 250 pixel diameter
+  // with its center at x position 200
+  // and at y position 200
+  ellipse(200,200,250,250);
+  stroke("Gray");
+  fill("white");
+  ellipse(150,150,75,75);
+  ellipse(250,150,75,75);
+  fill("black");
+  stroke("black");
+  ellipse(150,130,25,25);
+  ellipse(250,130,25,25);
+  rect(150,250,100,12,10,10,10,10);
 }
