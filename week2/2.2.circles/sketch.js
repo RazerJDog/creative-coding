2df11ca3a0b8af2f78fe62703f7db3087e7e6eca
RazerJDog <JUSTIN.RAZE.50@GMAIL.COM>
2023-05-23 13:15:18 -0400

    completed circles assignment

--- week2/2.2.circles/sketch.js
+++ week2/2.2.circles/sketch.js
@@ -1,7 +1,20 @@
 function setup() {
   createCanvas(1200, 1200);
+  // noloop();
 }
 
 function draw() {
   background(220);
+  translate(100,100);
+   for(let x=0; x<8;x++){
+     for(let y=0;y<8;y++){
+      push();
+      translate(x * 100, y * 100);
+      fill("purple");
+      ellipse(0,0,100,100);
+      fill("yellow");
+      triangle(0,-50,-43,25,43,25);
+      pop();
+     }
+  }
 }
