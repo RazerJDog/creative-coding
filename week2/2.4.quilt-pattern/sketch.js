543a2815adfa9090d5091c7ec5515aafedad879c
RazerJDog <JUSTIN.RAZE.50@GMAIL.COM>
2023-05-24 14:08:48 -0400

    completed quilt assignment

--- week2/2.4.quilt-pattern/sketch.js
+++ week2/2.4.quilt-pattern/sketch.js
@@ -1,7 +1,41 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1400, 1400);
+  noLoop();
 }
 
 function draw() {
-  background(220);
+  background("maroon");
+  angleMode(DEGREES);
+  translate(100,100);
+  for (let x=0;x<11;x++){
+    for (let y=0;y<11;y++){
+      push();
+      translate(x * 100, y * 100);
+      if (x%2==0 && y%2==0){
+        fill("gray");
+        rect(0, 0, 33, 100);
+        fill("black");
+        rect(33, 0, 33, 100);
+      }
+      else if(x%2==0 && y%2==1){
+        fill("gray");
+        rect(0,0,100,33);
+        fill("black");
+        rect(0,33,100,33);
+      }
+      else if(x%2==1 && y%2==0){
+        fill("gray");
+        rect(0,0,100,33);
+        fill("black");
+        rect(0,33,100,33);
+      }
+      else{
+        fill("gray");
+        rect(0, 0, 33, 100);
+        fill("black");
+        rect(33, 0, 33, 100);
+      }
+      pop();
+    }
+  }
 }
