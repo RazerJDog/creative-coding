fc8b4f0f4300ee535eb6f5ce1f9f60598e4ddeaf
RazerJDog <JUSTIN.RAZE.50@GMAIL.COM>
2023-05-25 13:18:23 -0400

    completed emulation assignment

--- week2/2.3.emulation/sketch.js
+++ week2/2.3.emulation/sketch.js
@@ -1,7 +1,45 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(700, 700);
+  noLoop();
 }
 
 function draw() {
-  background(255);
+  background("#f9f1e0");
+  translate(100,100);
+  angleMode(DEGREES);
+  for(let x=0;x<5;x++){
+    for(let y=0;y<5;y++){
+      push();
+      translate(x*100,y*100);
+      if(x%2==0 && y%2==0){
+        fill("#aa2e32");
+        rect(0,0,33,100);
+        rect(33,33,33,33);
+        rect(66,0,33,100);
+      }
+      if(x%2==0 && y%2==1){
+        fill("#e24426");
+        translate(100,0);
+        rotate(90);
+        rect(0,0,33,100);
+        rect(33,33,33,33);
+        rect(66,0,33,100);
+      }
+      else if(x%2==1 && y%2==0){
+        fill("#e24426");
+        translate(100,0);
+        rotate(90);
+        rect(0,0,33,100);
+        rect(33,33,33,33);
+        rect(66,0,33,100);
+      }
+      else{
+        fill("#aa2e32");
+        rect(0,0,33,100);
+        rect(33,33,33,33);
+        rect(66,0,33,100);
+      }
+      pop();
+    }
+  }
 }
