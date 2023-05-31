4467db561c8461bdc58da00e4b1ba2a07ddf8c82
RazerJDog <JUSTIN.RAZE.50@GMAIL.COM>
2023-05-31 12:32:42 -0400

    upload image to follower

--- week3/3.2.follower/sketch.js
+++ week3/3.2.follower/sketch.js
@@ -1,7 +1,22 @@
+let x = 200;
+let y = 200;
+let sprite;
+function preload(){
+  sprite = loadImage('https://razerjdog.github.io/creative-coding/week3/3.2.follower/Clyde.jpg');
+}
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+
+  text("width: " + width, 30, 30);
+  text("height: " + height, 30, 45);
+  text("mouseX: " + mouseX, 30, 60);
+  text("mouseY: " + mouseY, 30, 75);
+
+  let d = sqrt((x - mouseX)**2 + (y - mouseY)**2);
+
+
 }
